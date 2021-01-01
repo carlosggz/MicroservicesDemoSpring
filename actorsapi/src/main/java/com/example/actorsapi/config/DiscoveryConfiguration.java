d20229c88cf9826cb0fc1944493fb202d9d8ae27
Carlos Garcia <carlosggz@gmail.com>
2021-01-01 21:25:01 +0100

    Added discovery server and client configuration

---
+++ actorsapi/src/main/java/com/example/actorsapi/config/DiscoveryConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.actorsapi.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+//@Profile("default")
+@EnableDiscoveryClient
+@Configuration
+public class DiscoveryConfiguration {
+}
---
+++ moviesapi/src/main/java/com/example/moviesapi/config/DiscoveryConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.moviesapi.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//@Profile("default")
+@EnableDiscoveryClient
+@Configuration
+public class DiscoveryConfiguration {
+}
